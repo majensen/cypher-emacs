fafb0c95243571e952b114a9d2fc30c348fae172
Mark A. Jensen <mark.jensen@nih.gov>
2017-11-23 20:55:27 -0500

    feat(font-lock) : Seems to work
    additional functions for sending to comint buffer
    (ripped from sql.el) WIP

--- cypher-mode.el
+++ cypher-mode.el
@@ -1,9 +1,123 @@
+;; info nodes:
+;; Search-based Fontification
+;; Faces for Font Lock
+
 ;; for a cypher-mode - syntax elements
 ;; reserved words
 ;; functions
 ;; cli commands
 
 ;; shamelessly ripped off from sql-mode
+
+
+;; for resetting
+(makunbound 'cypher-kw)
+(makunbound 'cypher-fn)
+(makunbound 'cypher-cmd)
+(makunbound 'cypher-font-lock-keywords)
+
+(defvar cypher-buffer nil
+  "Current cypher-shell buffer.")
+
+;; words
+
+(defvar cypher-kw
+  '("add" "all" "and" "any" "as" "asc" "ascending" "assert" "by" "call"
+    "case" "commit" "constraint" "contains" "create" "csv"
+    "cypher" "delete" "desc" "descending" "detach" "distinct" "do" "drop"
+    "else" "end" "ends" "explain" "extract" "false"
+    "fieldterminator" "filter" "for" "foreach" "from" "headers" "in"
+    "index" "is" "join" "limit" "load" "mandatory" "match" "merge" "node"
+    "none" "not" "null" "of" "on" "optional" "or" "order" "periodic"
+    "profile" "reduce" "rel" "relationship" "remove" "require" "return"
+    "scalar" "scan" "set" "single" "skip" "start" "starts" "then" "true"
+    "u" "union" "unique" "unwind" "using" "when" "where" "with" "xor"
+    "yield" )
+  "Cypher keywords")
+
+(defvar cypher-fn
+  '("abs" "acos" "allShortestPaths" "asin" "atan" "atan2" "ceil" "coalesce"
+    "collect" "cos" "cot" "count" "degrees" "e" "exists" "exp" "floor"
+    "haversin" "head" "id" "keys" "labels" "last" "left" "length" "log"
+    "log10" "lower" "ltrim" "max" "min" "nodes" "percentileCont"
+    "percentileDisc" "pi" "radians" "rand" "range" "relationships"
+    "replace" "reverse" "right"	"round" "rtrim" "shortestPath" "sign"
+    "sin" "size" "split" "sqrt" "stDev" "stDevP" "substring" "sum"
+    "tail" "tan" "toInt" "toString" "trim" "type" "upper")
+  "Cypher built-in functions")
+
+(defvar cypher-cmd
+  '("begin" "commit" "exit" "help" "history" "param" "params" "rollback")
+  "Cypher-shell commands")
+
+(defface font-lock-type-bold
+  '((t :inherit (bold font-lock-type-face)))
+  "Type face bolded")
+(setq font-lock-type-bold 'font-lock-type-bold) ; this is weird that I have to do this
+
+
+(let* 
+;; matchers
+;; matcher for labels
+;; matcher for relationships <-- --> -- <-[]- -[]-> -[]-
+;; matcher for nodes (<token>)
+    ( (kw-re (regexp-opt cypher-kw))
+      (fn-re (regexp-opt cypher-fn))
+      (cmd-re (regexp-opt cypher-cmd))
+      (var-re "\\b[0-9A-Za-z_]+\\b")
+      )
+  (defconst cypher-font-lock-keywords
+    `(;; cypher commands first
+      (,(concat "\\b:" cmd-re "\\b") . font-lock-constant-face)
+      ;; then keywords
+      ,(concat "\\b" kw-re "\\b")
+      (,(concat "\\b" fn-re "\\b") . font-lock-function-name-face)
+      ;; relationship patterns
+      ( ")\\(<?-\\(:?\\[[^]]*\\]\\)?->?\\)("
+	1  font-lock-type-face)
+      ;; node patterns
+      (,(concat "\\W\\((" var-re "\\)\\([^()]*\\)\\()\\)")
+       (1  font-lock-type-bold)
+       (3 font-lock-type-bold))
+      ;; then labels
+      ( ,(concat ":" var-re) . font-lock-constant-face)
+
+      )))
+
+;; Font Lock
+
+(defun cypher-font-lock (keywords-only)
+  "Configure font-lock.
+
+The KEYWORDS-ONLY flag is passed to font-lock to specify whether
+only keywords should be highlighted and syntactic highlighting
+skipped."
+
+    ;; Setup font-lock.  Force re-parsing of `font-lock-defaults'.
+    (kill-local-variable 'font-lock-set-defaults)
+    (set (make-local-variable 'font-lock-defaults)
+         (list 'cypher-font-lock-keywords
+               keywords-only t nil))
+
+    ;; Force font lock to reinitialize if it is already on
+    ;; Otherwise, we can wait until it can be started. 
+   (when (and (fboundp 'font-lock-mode)
+	       (boundp 'font-lock-mode)
+	       font-lock-mode)
+      (font-lock-mode-internal nil)
+      (font-lock-mode-internal t))
+
+    (add-hook 'font-lock-mode-hook
+	      #'(lambda ()
+                  ;; Provide defaults for new font-lock faces.
+                  (defvar font-lock-builtin-face
+                    (if (boundp 'font-lock-preprocessor-face)
+                        font-lock-preprocessor-face
+                      font-lock-keyword-face))
+                  (defvar font-lock-doc-face font-lock-string-face))
+	      nil t))
+
+
 
 ;; Var
 
@@ -99,129 +213,96 @@ Based on `comint-mode-map'.")
     table)
   "Syntax table used in `cypher-mode' and `cypher-interactive-mode'.")
 
-
-;; Font Lock
-
-(defvar cypher-mode-reserved-font-lock-keywords nil
-  "Reserved words in Cypher.")
-(defvar cypher-mode-function-font-lock-keywords nil
-  "Built-in Cypher functions.")
-(defvar cypher-mode-command-font-lock-keywords nil
-  "Cypher-shell commands.")
-(defvar cypher-mode-font-lock-keywords nil
-  "All Cypher keywords.")
-
-(defun cypher-font-lock (keywords-only)
-  "Configure font-lock.
-
-The KEYWORDS-ONLY flag is passed to font-lock to specify whether
-only keywords should be highlighted and syntactic highlighting
-skipped."
-
-    ;; Setup font-lock.  Force re-parsing of `font-lock-defaults'.
-    (kill-local-variable 'font-lock-set-defaults)
-    (set (make-local-variable 'font-lock-defaults)
-         (list 'cypher-mode-font-lock-keywords
-               keywords-only t nil))
-
-    ;; Force font lock to reinitialize if it is already on
-    ;; Otherwise, we can wait until it can be started.
-    (when (and (fboundp 'font-lock-mode)
-	       (boundp 'font-lock-mode)
-	       font-lock-mode)
-      (font-lock-mode-internal nil)
-      (font-lock-mode-internal t))
-
-    (add-hook 'font-lock-mode-hook
-	      #'(lambda ()
-                  ;; Provide defaults for new font-lock faces.
-                  (defvar font-lock-builtin-face
-                    (if (boundp 'font-lock-preprocessor-face)
-                        font-lock-preprocessor-face
-                      font-lock-keyword-face))
-                  (defvar font-lock-doc-face font-lock-string-face))
-	      nil t))
-
-(defun cypher-font-lock-keywords-builder (face boundaries &rest keywords)
-  "Generation of regexp matching any one of KEYWORDS."
-  
-  (let ((bdy (or boundaries '("\\b" . "\\b")))
-	kwd)
-    ;; pre-processing of keywords here
-    ;; (dolist (k keywords) ... )
-    (setq kwd keywords)
-    ;; Create a properly formed font-lock-keywords item
-    (cons (concat (car bdy)
-		  (regexp-opt kwd t)
-		  (cdr bdy))
-	  face)))
-
-(setq cypher-mode-reserved-font-lock-keywords
-      (list
-       ;; Cypher reserved words
-       (cypher-font-lock-keywords-builder
-	'font-lock-keyword-face nil
- ;;; keyword list
-	"add" "all" "and" "any" "as" "asc" "ascending" "assert" "by" "call"
-	"case" "commit" "constraint" "contains" "count" "create" "csv"
-	"cypher" "delete" "desc" "descending" "detach" "distinct" "do" "drop"
-	"else" "end" "ends" "exists" "explain" "extract" "false"
-	"fieldterminator" "filter" "for" "foreach" "from" "headers" "in"
-	"index" "is" "join" "limit" "load" "mandatory" "match" "merge" "node"
-	"none" "not" "null" "of" "on" "optional" "or" "order" "periodic"
-	"profile" "reduce" "rel" "relationship" "remove" "require" "return"
-	"scalar" "scan" "set" "single" "skip" "start" "starts" "then" "true"
-	"u" "union" "unique" "unwind" "using" "when" "where" "with" "xor"
-	"yield" )))
-
-(setq cypher-mode-function-font-lock-keywords
-      (list
-       ;; Cypher reserved words
-       (cypher-font-lock-keywords-builder
-	'font-lock-builtin-face nil
- ;;; function list
-	"abs" "acos" "allShortestPaths" "asin" "atan" "atan2" "ceil" "coalesce" "collect" "cos" "cot"
-	"count" "degrees" "e" "exists" "exp" "floor" "haversin" "head" "id" "keys" "labels" "last"
-	"left" "length" "log" "log10" "lower" "ltrim" "max" "min" "nodes" "percentileCont"
-	"percentileDisc" "pi" "radians" "rand" "range" "relationships" "replace" "reverse" "right"
-	"round" "rtrim" "shortestPath" "sign" "sin" "size" "split" "sqrt" "stDev" "stDevP" "substring"
-	"sum" "tail" "tan" "toInt" "toString" "trim" "type" "upper")))
-
-(setq cypher-mode-command-font-lock-keywords
-      (list
-       ;; cypher-shell commands
-       (cypher-font-lock-keywords-builder
-	'font-lock-keyword-face nil
- ;;; command list
-	":begin" ":commit" ":exit" ":help" ":history" ":param" ":params" ":rollback")))
-
-(setq cypher-mode-font-lock-keywords
-      (flatten (list
-		cypher-mode-reserved-font-lock-keywords
-		cypher-mode-function-font-lock-keywords
-		cypher-mode-command-font-lock-keywords)))
-      
-
-(defun cypher-interactive-mode (arg)
-  "Major mode for Neo4j cypher-shell CLI."
-  (interactive "P")
+;; Cypher interactive mode
+(put 'cypher-interactive-mode 'mode-class 'special)
+(put 'cypher-interactive-mode 'custom-mode-group 'Cypher)
+(defun cypher-interactive-mode ()
+  "Major mode to use cypher-shell interactively."
+  (delay-mode-hooks (comint-mode))
   (setq major-mode 'cypher-interactive-mode)
-  (setq mode-name "Cypher")
+  (setq mode-name "iCypher")
   (use-local-map cypher-interactive-mode-map)
   (set-syntax-table cypher-mode-syntax-table)
+  ;; start font lock
+  (cypher-font-lock nil)
   (setq local-abbrev-table cypher-mode-abbrev-table)
   (setq abbrev-all-caps 1)
-
-  ;; buffer local status variables
-
+  (set-process-sentinel (get-buffer-process (current-buffer)) 'cypher-stop)
+  ;; buffer local variables here
+  (make-local-variable 'cypher-prompt-regexp cypher-prompt-regexp)
+  (make-local-variable 'cypher-prompt-length cypher-prompt-length)
+  (make-local-variable 'cypher-preoutput-hold)  
   ;; run hooks
-  (run-mode-hooks 'cypher-interactive-mode-hook)
-  ;;
-  (setq comint-prompt-regexp
-	(if cypher-prompt-cont-regexp
-            (concat "\\(" cypher-prompt-regexp
-                    "\\|" cypher-prompt-cont-regexp "\\)")
-          cypher-prompt-regexp))
+  (run-mode-hooks 'cypher-interactive-mode-hooks)
+  ;; set comint
+  (setq cominit-prompt-regexp cypher-prompt-cont-regexp)
   (setq left-margin cypher-prompt-length)
-  (cypher-font-lock nil)
-)
+  ;; input sender?
+  )
+
+(defun cypher-stop (process event)
+  "Called after cypher-shell is killed."
+  (if (not buffer-read-only)
+      (insert (format "\nProcess %s %s\n" process event))
+    (message "Process %s %s" process event)))
+
+;;; Sending the region to the Cypher buffer.
+
+(defun cypher-send-string (str)
+  "Send the string STR to the Cypher-shell process."
+  (interactive "sCypher text: ")
+
+  (let ((comint-input-sender-no-newline nil)
+        (s (replace-regexp-in-string "[[:space:]\n\r]+\\'" "" str)))
+    (if (sql-buffer-live-p sql-buffer)
+	(progn
+	  ;; Ignore the hoping around...
+	  (save-excursion
+	    ;; Set product context
+	    (with-current-buffer sql-buffer
+	      ;; Send the string (trim the trailing whitespace)
+	      (sql-input-sender (get-buffer-process sql-buffer) s)
+
+	      ;; Send a command terminator if we must
+	      (if sql-send-terminator
+		  (sql-send-magic-terminator sql-buffer s sql-send-terminator))
+
+	      (message "Sent string to buffer %s" sql-buffer)))
+
+	  ;; Display the sql buffer
+	  (if sql-pop-to-buffer-after-send-region
+	      (pop-to-buffer sql-buffer)
+	    (display-buffer sql-buffer)))
+
+    ;; We don't have no stinkin' sql
+    (user-error "No SQL process started"))))
+
+(defun cypher-send-region (start end)
+  "Send a region to the CYPHER process."
+  (interactive "r")
+  (cypher-send-string (buffer-substring-no-properties start end)))
+
+(defun cypher-send-paragraph ()
+  "Send the current paragraph to the CYPHER process."
+  (interactive)
+  (let ((start (save-excursion
+		 (backward-paragraph)
+		 (point)))
+	(end (save-excursion
+	       (forward-paragraph)
+	       (point))))
+    (cypher-send-region start end)))
+
+(defun cypher-send-buffer ()
+  "Send the buffer contents to the CYPHER process."
+  (interactive)
+  (cypher-send-region (point-min) (point-max)))
+
+(defun cypher-send-line-and-next ()
+  "Send the current line to the CYPHER process and go to the next line."
+  (interactive)
+  (cypher-send-region (line-beginning-position 1) (line-beginning-position 2))
+  (beginning-of-line 2)
+  (while (forward-comment 1)))  ; skip all comments and whitespace
+
+
--- cypher.el
+++ cypher.el
@@ -68,11 +68,42 @@ Note: cypher-shell will use cypher-get-host-interactive to more conveniently get
   (interactive (cypher-get-host-interactive))
   (if (not (getenv "NEO4J_HOME"))
       (setenv "NEO4J_HOME" neo4j-home))
-  (let ((proto (concat protocol "://"))
-	(cypher_bufname (concat "cypher : " host )))
-    (switch-to-buffer
-     (make-comint cypher_bufname cypher-prog "/dev/null" "-a"
-		  (concat proto host ":" port) ))
-    (cypher-interactive-mode nil)
-    ))
-  
+  (cypher-comint protocol host port)
+  (cypher-interactive-mode nil))
+
+(defun cypher-buffer-live-p (buffer)
+  "Return non-nil if the process associated with buffer is live.
+
+BUFFER can be a buffer object or buffer name."
+  (when buffer
+    (setq buffer (get-buffer buffer))
+    (and buffer
+	 (buffer-live-p buffer)
+	 (comint-check-proc buffer)
+	 (with-current-buffer buffer
+	   (derived-mode-p 'cypher-interactive-mode))
+	 )))
+	  
+(defun cypher-comint proto host port
+    "Set up comint buffer for cypher-shell.
+
+PROTO protocol (http https bolt)
+HOST url or ip
+PORT Cypher shell port"
+    (let (( pgm (executable-find cypher-prog))
+	  ( buf-name "Cypher"))
+    (unless pgm
+      (error "Can't find shell program %s" cypher-prog))
+    (when (cypher-buffer-live-p (format "*%s*" buf-name))
+      (let ((i 1))
+	(while (cypher-buffer-live-p
+		(format "*%s*"
+			(setq buf-name (format "Cypher-%d" i)))
+		(setq i (1+ i))))))
+    (set-buffer
+     (make-comint buf-name pgm "/dev/null" "-a"
+		  (concat (format "%s://" proto) host ":" port) ))))
+     
+    
+	
+     
